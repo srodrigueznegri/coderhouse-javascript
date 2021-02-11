fb1985d6e5bd2a2882a634f170bd7d2e1f34fd12
Stephanie Rodriguez Negri <rodrigueznegri.stephanie@gmail.com>
2021-02-10 22:38:24 -0300

    hice desafio 2 - condicionales

---
+++ main.js
@@ -0,0 +1,9 @@
+// alert("Hola! Quizas me conozcas como 'Stephi esta aburrida'")
+
+var price = Number(prompt("Ingresa un precio"));
+
+if ((price <= 1000) && (price >= 0)) {
+    alert("Que barato!");
+} else {
+    alert("Que caro!");
+}
